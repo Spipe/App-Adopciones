9a30da39cd6b9bb7a3a6c3531fe8984bc6798729
Javiera Vergara <javiera.vergaran@utem.cl>
2019-07-12 15:28:50 -0400

    Se agrega catálogo

---
+++ src/screens/catalogo.js
@@ -0,0 +1,44 @@
+
+Vue.component('info', {
+    props: {}
+    ,
+    template:`
+    <div class='mascota-display'>
+        <img v-bind:src="image">
+        <h2>{{ nombreA }}</h2>
+        <p v-if="estado">Disponible para adopcion</p>
+        <p v-else>Adoptado</p>
+    </div>`,
+    data() {
+        return {
+            nombreA : "Gatito",
+            product: 'Michi ',
+            image: '../assets/Chico_excremento.jpg',
+            esterilizado: true,
+            estado: true
+        }
+    },
+    methods: {
+        addInteresados() {
+            this.$emit('ingresar-gato')
+        }
+    },
+    computed: {
+        title() {
+            return this.nombreA
+        }
+    }
+
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        interesados: 0,
+    },
+    methods: {
+        updatemichis() {
+            this.interesados += 1
+        }
+    }
+});
